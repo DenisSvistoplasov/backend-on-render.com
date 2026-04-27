0b2ad6091f953487bf3f68c94b46c1270583ee5c
Denis <denissvistoplasov.work@gmail.com>
2026-04-27 16:53:16 +0400

    allow cors

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,18 @@ app.get('/api/ping', async (req: Request, res: Response) => {
   }
 });
 
-// Endpoints for P2P test 
+// Endpoints for P2P test
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 const p2pData = {
   offer: null as any,
   answer: null as any
