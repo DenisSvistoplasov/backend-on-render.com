b82ae5763079bb0bd85ff19e99d1937728b2a7b9
Denis <denissvistoplasov.work@gmail.com>
2026-07-06 00:27:11 +0400

    remove ping

--- src/webSocket/webSocket.ts
+++ src/webSocket/webSocket.ts
@@ -17,11 +17,11 @@ export function startWebSocket(
   wss.on('connection', (ws: WebSocket) => {
     console.log('Client connected WS');
 
-    let pingInterval = setInterval(() => {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.ping();
-      }
-    }, 30_000);
+    // let pingInterval = setInterval(() => {
+    //   if (ws.readyState === WebSocket.OPEN) {
+    //     ws.ping();
+    //   }
+    // }, 30_000);
 
     ws.on('pong', () => {
       // Обновляем время жизни соединения
@@ -34,12 +34,12 @@ export function startWebSocket(
     ws.on('close', () => {
       console.log('Client disconnected');
       onClose(ws);
-      clearInterval(pingInterval);
+      // clearInterval(pingInterval);
     });
 
     ws.on('error', (error: Error) => {
       console.error('WebSocket error:', error);
-      clearInterval(pingInterval);
+      // clearInterval(pingInterval);
     });
   });
 
